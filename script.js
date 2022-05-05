17f10a5a2796cf59937c6d4b8ec5c5b843c9902a
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-05 19:30:15 -0300

    reorganizando codigo stile e css

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-
 // FUNÇÃO QUE VERIFICA DADOS DE EMAIL E SENHA
 function login() {
-    let email = document.querySelector('#campoEmail');
-    let senha = document.querySelector('#campoSenha');
-    if (email.value === 'tryber@teste.com' && senha.value === '123456') {
-        alert('Olá, Tryber!')
-    } else {
-        alert('Email ou senha inválidos.')
-    }
+  const email = document.querySelector('#campoEmail');
+  const senha = document.querySelector('#campoSenha');
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
 
 const btnLogin = document.querySelector('#login');
